f59d7d26be4425f955d9d3baadfc958f5ea3cff9
Asset Malik <asetmalik@gmail.com>
2021-11-01 11:47:46 +0600

    before adding EasyTransfer

--- AlarisHandCubeIDE/Debug/Core/Startup/subdir.mk
+++ AlarisHandCubeIDE/Debug/Core/Startup/subdir.mk
@@ -15,6 +15,6 @@ OBJS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Core/Startup/startup_stm32f103c6tx.o: ../Core/Startup/startup_stm32f103c6tx.s Core/Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -c -x assembler-with-cpp -MMD -MP -MF"Core/Startup/startup_stm32f103c6tx.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
 
